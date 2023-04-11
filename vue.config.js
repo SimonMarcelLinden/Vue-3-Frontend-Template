76c02a9d318812add1a8976dc0dbe27c558869ea
Simon Marcel Linden <linden@snerve.de>
2023-04-11 16:05:22 +0200

    Added some new packages an reconfig vue config
    Added vuex-class, axios, @types/lodash, bootstrap-icons, style-loader, vue-loader & node-sass
    
    Configured Vue-Config to use SourceMap at CSS. Disabled css modules and scoped option

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+	transpileDependencies	: true,
+	lintOnSave				: false,
+	css: {
+		loaderOptions: {
+			css: {
+				sourceMap: true,
+				modules: false
+			},
+			sass: {
+				sourceMap: true,
+				// additionalData: `@import "@/scss/index.scss";`,
+				sassOptions: {
+					scoped: false // Deaktiviert das Scoped-Verhalten
+				},
+				implementation: require('node-sass')
+			},
+			scss: {
+				sourceMap: true,
+				// additionalData: `@import "@/scss/index.scss";`,
+				sassOptions: {
+					scoped: false // Deaktiviert das Scoped-Verhalten
+				},
+				implementation: require('node-sass')
+			}
+		}
+	},
+}
