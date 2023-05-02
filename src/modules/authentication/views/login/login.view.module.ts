7f1a4afaa01e44dc64a76597d51794635ba3828a
Simon Marcel Linden <linden@snerve.de>
2023-05-02 13:13:19 +0200

    Interface for errors and credentials.
    Interface created for errors and credentials. Also removed old code snippets.
    
    Interface für Error und Credentials erstellt. Zusätzlich alte Code Snippets entfernt.

--- src/modules/authentication/views/login/login.view.module.ts
+++ src/modules/authentication/views/login/login.view.module.ts
@@ -1,28 +1,33 @@
 import { Vue } from 'vue-class-component'
 import { namespace } from 'vuex-class';
-import router from '@/router';
 
-const Global 			= namespace('global');
 const Authentication 	= namespace('authentication');
-const lang 				= localStorage.getItem('language') || 'en';
 
 const body 				= document.getElementsByTagName("body")[0];
 
+interface Error {
+	field	: string | null;
+	title	: string | null;
+	message	: string | null;
+}
+
+interface Credentials {
+	email	: string | null;
+	password: string | null;
+}
+
 export default class Login extends Vue {
-	@Global.Action('loading') 			private loading : any
 	@Authentication.Action('login') 	private login	: any;
 
-	// @ts-ignore-next-line
-	private error: Array = {
+	private error: Error = {
 		field	: null,
 		title	: null,
 		message	: null
 	};
 
-	// Todo: remove value on finale Version
-	private credentials: any = {
-		email	: 'admin@schulz-hygiene.de',
-		password: 'password',
+	private credentials: Credentials = {
+		email	: null,
+		password: null,
 	};
 
 	public created() {
@@ -31,17 +36,12 @@ export default class Login extends Vue {
 
 	public async handleSignIn() {
 		const { email, password } = this.credentials;
-		this.loading(true);
-
 		try {
 			await this.login(this.credentials)
-			router.replace('/');
 		} catch (error: any) {
 			this.error.field 	= Object.keys( error.response.data.data )[0]
 			this.error.title 	= error.response.data.detail;
 			this.error.message 	= error.response.data.data[this.error.field]['en']['message'];
 		}
-
-		this.loading(false);
 	}
 }
