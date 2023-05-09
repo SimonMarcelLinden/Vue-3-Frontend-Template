a3ce9680f82db93d65d73919cb6d5a44d277a866
Simon Marcel Linden <linden@snerve.de>
2023-05-09 14:02:52 +0200

    Delete Files
    Deleted views that are not required.
    
    Nicht benötigte Views gelöscht.

--- src/views/home-view/home-view.view.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<h1>Hello World</h1>
-	<sharedExample text="This is an example text for Shared Example Component"></sharedExample>
-</template>
-
-<style src="./home-view.view.scss" lang="scss"/>
-<script src="./home-view.view.ts"></script>
--- src/views/main-view.view.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<router-view/>
-</template>
-
-<style src="./main-view.view.scss" lang="scss"/>
-<script src="./main-view.view.ts"></script>
